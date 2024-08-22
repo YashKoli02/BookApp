021518c7ad209a1bbbe5e2569ac06929944423ed
Yash Koli <96144746+YashKoli02@users.noreply.github.com>
2024-08-23 04:45:40 +0530

    Update Login.jsx

--- Backend/Frontend/src/components/Login.jsx
+++ Backend/Frontend/src/components/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
       email:data.email,
       password:data.password,
     }
-   await axios.post("/user/login",userInfo)
+   await axios.post("https://bookapp-ktia.onrender.com/user/login",userInfo)
     .then((res)=>{
       console.log(res.data)
       if(res.data){
